98bfb65367e843f248858bd247ba3e2eb8622960
Murilo Molina Barone <murilo.m.barone@gmail.com>
2025-01-10 15:41:40 -0300

    Initial version of the documentation

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,42 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>SmartCampus Mauá Docs</span>,
+  
+  // project: {
+  //   link: 'https://github.com/SmartCampusMaua/smartcampusmaua-nextrav2',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // footer: {
+  //   text: 'Nextra Docs Template',
+  // },
+  feedback: {
+    content: null, // Removes the feedback link
+
+    // content: (
+    //   <a
+    //     href="https://smartcampus.maua.br/"
+    //     target="_blank"
+    //     rel="noopener noreferrer"
+    //   >
+    //     Question? Give us feedback →
+    //   </a>
+    // ),
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
+  editLink: {
+    text: null, // Ensures no text is displayed for the edit link
+  //   component: ({ filePath }) => (
+  //     <a
+  //       href={`https://github.com/SmartCampusMaua/smartcampusmaua-nextrav2`}
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       Edit this page
+  //     </a>
+  //   ),
   },
 }
 
